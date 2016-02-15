c0762803be6560db3417c47849d01845e8bb80a4
Jianguo_Zhang <jgzhang.cross@foxmail.com>
2016-02-15 12:43:16 +0800

    code for wifi smartlink, which has been tested in the project

---
+++ EvalBoards/ST/STM32F4-Discovery/uCOS-III/App/src/smartlink.h
@@ -0,0 +1,26 @@
+#ifndef _SMARTLINK_H
+#define _SMARTLINK_H
+
+
+#include "rt_config.h"
+
+
+
+int smartlink_start(RTMP_ADAPTER *pAd);
+
+int smartlink_stop(RTMP_ADAPTER *pAd);
+
+int smartlink_init(void);
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
